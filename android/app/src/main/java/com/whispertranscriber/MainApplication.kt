d6af7eacf808c9eaee8c603a2b0bca2e3d3c2bf6
Ninjaultrazero <pasqualematrone2007@gmail.com>
2026-05-09 13:07:34 +0200

    refactor: update MainApplication to support React Native New Architecture and standard autolinking setup

--- android/app/src/main/java/com/whispertranscriber/MainApplication.kt
+++ android/app/src/main/java/com/whispertranscriber/MainApplication.kt
@@ -3,10 +3,12 @@ package com.whispertranscriber
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.internal.featureflags.ReactNativeFeatureFlags
 import com.facebook.soloader.SoLoader
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
@@ -14,21 +16,30 @@ class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    // Autolink gestisce tutto
-                    add(WhisperPackage())
-                }
+            override fun getPackages(): List<ReactPackage> {
+                // Autolinking recupera automaticamente WhisperPackage e gli altri
+                return PackageList(this).packages
+            }
+
+            override fun getJSMainModuleName(): String = "index"
 
             override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            override val isNewArchEnabled: Boolean = false
-            override val isHermesEnabled: Boolean = true
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
         }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
-
+        // Inizializza SoLoader con il mapping corretto per evitare UnsatisfiedLinkError
         SoLoader.init(this, OpenSourceMergedSoMapping)
+        
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // Carica la libreria nativa per la New Architecture se abilitata
+            load()
+        }
     }
 }
