00ee1508a3abc97056c6d852ca021f74dd0962a7
Ninjaultrazero <pasqualematrone2007@gmail.com>
2026-05-09 12:47:29 +0200

    fix initialization (?)

--- android/app/src/main/java/com/whispertranscriber/MainApplication.kt
+++ android/app/src/main/java/com/whispertranscriber/MainApplication.kt
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.internal.featureflags.ReactNativeFeatureFlags
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -28,6 +29,6 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
 
-        SoLoader.init(this, false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)
     }
 }
