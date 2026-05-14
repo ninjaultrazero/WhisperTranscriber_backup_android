ef033cee43378f7fccc4b8c0275ef087110bb7c8
pako <pasqualematrone2007@gmail.com>
2026-05-14 09:31:44 +0200

    tryng to fix the 4kb page size and the missing lib when installed

--- android/app/src/main/java/com/whispertranscriber/MainApplication.kt
+++ android/app/src/main/java/com/whispertranscriber/MainApplication.kt
@@ -10,7 +10,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
