021a8add07e0621aa5f2e3f2e13a76f57bd02dc7
pako <pasqualematrone2007@gmail.com>
2026-05-14 09:53:13 +0200

    installed the app successfull

--- android/app/src/main/java/com/whispertranscriber/MainApplication.kt
+++ android/app/src/main/java/com/whispertranscriber/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -33,7 +34,9 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        // Inizializzazione corretta per React Native 0.74+
-        SoLoader.init(this, false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
     }
 }
